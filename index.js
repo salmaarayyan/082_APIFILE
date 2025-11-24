665ad9dfc58ec4323eb06f1f118644186f97720f
salmaarayyan <salmaarayyan@gmail.com>
2025-11-24 10:16:21 +0700

    Menambahkan route API untuk versi 1 di server Express

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const connectDatabase = require('./config/db');
+const apiRoutes = require('./routes/api');
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.use('/api/v1', apiRoutes);
 
 async function startServer() {
     await connectDatabase();
